cf2010ea7284e40c04b76b38659a71f53258cb5e
deffinad <deffinjr890@gmail.com>
2023-03-13 01:53:12 -0700

    Add Head

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import { Navbar } from "../navbar";
 
 export const Layout = (props: any) => {
   const { children } = props;
   return (
     <div>
+      <Head>
+        <title>Todo Lists</title>
+      </Head>
       <div className="container">
         <div className="main-content">
           <Navbar />
